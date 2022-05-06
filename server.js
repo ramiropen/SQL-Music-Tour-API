6eb31404b3fd779a97e18d91ce35b4d65c866426
unknown <ramiropen.work@gmail.com>
2022-05-05 20:03:44 -0700

    setup

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const express = require("express");
+const { Sequelize } = require("sequelize");
+const app = express();
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI);
+
+try {
+  sequelize.authenticate();
+  console.log(`Connected with Sequelize at ${process.env.PG_URI}`);
+} catch (err) {
+  console.log(`Unable to connect to PG: ${err}`);
+}
 
 // ROOT
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Tour API",
+  });
+});
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
